0e90a84728ed9f9e3c2e1b322cf1a99e29093436
SURYANSH SHARMA <suryanshnadda@gmail.com>
2018-11-23 05:10:46 -0800

    Create arraylist.java
    arraylist storing multi type values

---
+++ arraylist.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class nody{
+
+     public static void main(String []args)
+     {
+     
+    // String a[]={"son",1,1};
+     
+     ArrayList b = new ArrayList();
+     b.add(1);
+     b.add(1);
+     b.add("aa");
+     
+     for(int i=0;i<b.size();i++)
+     {
+         System.out.println(b.get(i));
+     }
+     
+     }
+}
